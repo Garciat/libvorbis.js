0111847ebeb2e8a63c324f0abb5008a09db833d2
Gabriel Garcia <garciat@live.com>
2015-07-27 21:01:35 -0400

    organize, format

--- gulpfile.js
+++ gulpfile.js
@@ -12,25 +12,7 @@ var tsc     = require('gulp-typescript');
 
 var CONFIG = {
     cc: 'emcc',
-    cc_flags: '-O3 -ffast-math -Inative/include',
-    libogg: {
-        flags: '-Ilibogg/src -Ilibogg/include',
-        outDir: 'build/libogg',
-        srcDir: 'libogg/src',
-        src: 'bitwise.c framing.c'.split(' ')
-    },
-    libvorbis: {
-        flags: '-Ilibogg/src -Ilibogg/include -Ilibvorbis/lib -Ilibvorbis/include',
-        outDir: 'build/libvorbis',
-        srcDir: 'libvorbis/lib',
-        src: 'analysis.c bitrate.c block.c codebook.c envelope.c floor0.c floor1.c info.c lookup.c lpc.c lsp.c mapping0.c mdct.c psy.c registry.c res0.c sharedbook.c smallft.c synthesis.c vorbisenc.c window.c'.split(' ')
-    },
-    ogg_vbr: {
-        flags: '-Ilibogg/src -Ilibogg/include -Ilibvorbis/lib -Ilibvorbis/include',
-        outDir: 'build/native/ogg_vbr',
-        srcDir: 'native/ogg_vbr',
-        src: ['encoder.c']
-    }
+    cc_flags: '-O3 -ffast-math -Inative/include'
 };
 
 function compile(native) {
@@ -58,29 +40,49 @@ gulp.task('clean', function () {
     del('dist');
 });
 
-gulp.task('libogg', function () {
-    return compile(CONFIG.libogg);
+gulp.task('amsjs_libogg', function () {
+    return compile({
+        flags: '-Ilibogg/src -Ilibogg/include',
+        outDir: 'build/libogg',
+        srcDir: 'libogg/src',
+        src: ['bitwise.c', 'framing.c']
+    });
 });
 
-gulp.task('libvorbis', function () {
-    return compile(CONFIG.libvorbis);
+gulp.task('amsjs_libvorbis', function () {
+    return compile({
+        flags: '-Ilibogg/src -Ilibogg/include -Ilibvorbis/lib -Ilibvorbis/include',
+        outDir: 'build/libvorbis',
+        srcDir: 'libvorbis/lib',
+        src: ["analysis.c", "bitrate.c", "block.c", "codebook.c", "envelope.c",
+              "floor0.c", "floor1.c", "info.c", "lookup.c", "lpc.c", "lsp.c",
+              "mapping0.c", "mdct.c", "psy.c", "registry.c", "res0.c",
+              "sharedbook.c", "smallft.c", "synthesis.c", "vorbisenc.c",
+              "window.c"]
+    });
 });
 
-gulp.task('ogg_vbr', function () {
-    return compile(CONFIG.ogg_vbr)
+gulp.task('amsjs_ogg_vbr', function () {
+    return compile({
+        flags: '-Ilibogg/src -Ilibogg/include -Ilibvorbis/lib -Ilibvorbis/include',
+        outDir: 'build/native/ogg_vbr',
+        srcDir: 'native/ogg_vbr',
+        src: ['encoder.c']
+    })
 });
 
-gulp.task('native', ['libogg', 'libvorbis', 'ogg_vbr']);
+gulp.task('asmjs_modules', ['amsjs_libogg', 'amsjs_libvorbis', 'amsjs_ogg_vbr']);
 
-gulp.task('libvorbis.asmjs', ['native'], function (done) {
+function compileModule(flags, dest, done) {
     glob('build/**/*.bc', function (err, files) {
         var bcs = files.join(' ');
         
-        var cmd = [CONFIG.cc, '-O1 -s ALLOW_MEMORY_GROWTH=0 -s ASM_JS=1',
+        var cmd = [CONFIG.cc, flags,
+                   '-s ALLOW_MEMORY_GROWTH=0 -s ASM_JS=1',
                    '-s EXPORTED_FUNCTIONS=@exported_functions.json',
                    '--pre-js modules/libvorbis/src/asmjs/prefix.js',
                    '--post-js modules/libvorbis/src/asmjs/suffix.js',
-                   bcs, '-o dist/js/libvorbis.asmjs.js'].join(' ');
+                   bcs, '-o', dest].join(' ');
         
         mkdirp.sync('dist/js');
         
@@ -88,24 +90,14 @@ gulp.task('libvorbis.asmjs', ['native'], function (done) {
             .pipe(shell(cmd))
             .on('end', done);
     });
+}
+
+gulp.task('asmjs', ['asmjs_modules'], function (done) {
+    compileModule('-O1', 'dist/js/libvorbis.asmjs.js', done);
 });
 
-gulp.task('libvorbis.asmjs.min', ['native'], function (done) {
-    glob('build/**/*.bc', function (err, files) {
-        var bcs = files.join(' ');
-        
-        var cmd = [CONFIG.cc, '-O3 -s ALLOW_MEMORY_GROWTH=0 -s ASM_JS=1',
-                   '-s EXPORTED_FUNCTIONS=@exported_functions.json',
-                   '--pre-js modules/libvorbis/src/asmjs/prefix.js',
-                   '--post-js modules/libvorbis/src/asmjs/suffix.js',
-                   bcs, '-o dist/js/libvorbis.asmjs.min.js'].join(' ');
-        
-        mkdirp.sync('dist/js');
-        
-        gulp.src('gulpfile.js') // we just need a file to run the command once
-            .pipe(shell(cmd))
-            .on('end', done);
-    });
+gulp.task('asmjs.min', ['asmjs_modules'], function (done) {
+    compileModule('-O3', 'dist/js/libvorbis.asmjs.min.js', done);
 });
 
 gulp.task('libvorbis.js.cjs', function () {
@@ -113,18 +105,18 @@ gulp.task('libvorbis.js.cjs', function () {
     
     var job = proj.src().pipe(tsc(proj));
     
-    return merge(job.js.pipe(gulp.dest('dist/js/libvorbis')),
-                 job.dts.pipe(gulp.dest('dist/js/libvorbis')));
+    return merge(job.js.pipe(gulp.dest('dist/cjs/libvorbis')),
+                 job.dts.pipe(gulp.dest('dist/dts/libvorbis')));
 });
 
 gulp.task('libvorbis.js', ['libvorbis.js.cjs'], function () {
     return new Builder({
-        paths: { '*': 'dist/js/*.js' },
+        paths: { '*': 'dist/cjs/*.js' },
         defaultJSExtensions: true
     })
     .build('libvorbis/libvorbis', 'dist/js/libvorbis.js');
 });
 
-gulp.task('all', ['libvorbis.js', 'libvorbis.asmjs', 'libvorbis.asmjs.min']);
+gulp.task('all', ['libvorbis.js', 'asmjs', 'asmjs.min']);
 
 gulp.task('default', ['all']);
